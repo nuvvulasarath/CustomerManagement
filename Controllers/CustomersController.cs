7fe449497ae0fb9232286549f165d25a15dc5e6d
nuvvula sarath <37181013+nuvvulasarath@users.noreply.github.com>
2023-07-10 00:13:43 +0200

    Add project files.

---
+++ Controllers/CustomersController.cs
@@ -0,0 +1,51 @@
+﻿using CustomerManagement.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
+using System.Linq;
+using CustomerManagement.Helper;
+
+namespace CustomerManagement.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CustomersController : ControllerBase
+    {
+        private static List<Customer> customers = new List<Customer>();
+        public CustomersController()
+        {
+            customers = CustomersHelper.GetCustomersFromFile();
+        }
+
+        // POST api/customers
+        [HttpPost]
+        public IActionResult Post([FromBody] List<Customer> newCustomers)
+        {
+            if (newCustomers == null || !newCustomers.Any())
+            {
+                return BadRequest("Invalid request");
+            }
+
+            var validCustomers = new List<Customer>();
+
+            foreach (var customer in newCustomers)
+            {
+                if (CustomersHelper.ValidateCustomer(customer, customers))
+                {
+                    validCustomers.Add(customer);
+                    CustomersHelper.InsertCustomer(customer, customers);
+                }
+            }
+
+            CustomersHelper.SaveCustomersToFile(customers);
+
+            return StatusCode(201, new { Message = "Customers added successfully", Customers = validCustomers });
+        }
+
+        // GET api/customers
+        [HttpGet]
+        public IActionResult Get()
+        {
+            return Ok(customers);
+        }
+    }
+}
---
+++ Helper/CustomersHelper.cs
@@ -0,0 +1,66 @@
+﻿using CustomerManagement.Models;
+using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CustomerManagement.Helper
+{
+    public static class CustomersHelper
+    {
+        private const string FilePath = "customers.json"; // need to replace with absolute path where there are initial customers list
+        public static List<Customer> GetCustomersFromFile()
+        {
+            var customers = new List<Customer>();
+            if (System.IO.File.Exists(FilePath))
+            {
+                var json = System.IO.File.ReadAllText(FilePath);
+                customers = JsonConvert.DeserializeObject<List<Customer>>(json);
+            }
+
+            return customers;
+        }
+
+        public static void SaveCustomersToFile(List<Customer> customers)
+        {
+            var json = JsonConvert.SerializeObject(customers);
+            System.IO.File.WriteAllText(FilePath, json);
+        }
+
+        public static bool ValidateCustomer(Customer customer, List<Customer> customers)
+        {
+            if (customer.Id <= 0 || customer.Age <= 0 || string.IsNullOrWhiteSpace(customer.FirstName) ||
+                string.IsNullOrWhiteSpace(customer.LastName))
+            {
+                return false;
+            }
+
+            if (customer.Age <= 18)
+            {
+                return false;
+            }
+
+            if (customers.Any(c => c.Id == customer.Id))
+            {
+                return false;
+            }
+
+            return true;
+        }
+
+        public static void InsertCustomer(Customer customer, List<Customer> customers)
+        {
+            int index = 0;
+            while (index < customers.Count &&
+                   (string.Compare(customers[index].LastName, customer.LastName, StringComparison.OrdinalIgnoreCase) < 0 ||
+                    (string.Compare(customers[index].LastName, customer.LastName, StringComparison.OrdinalIgnoreCase) == 0 &&
+                     string.Compare(customers[index].FirstName, customer.FirstName, StringComparison.OrdinalIgnoreCase) < 0)))
+            {
+                index++;
+            }
+
+            customers.Insert(index, customer);
+        }
+    }
+}
